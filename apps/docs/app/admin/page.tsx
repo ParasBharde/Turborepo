c2d313f40894682c088d6f61a7d7439e6932f280
paras_b <paras.barde2000@gmail.com>
2024-04-10 10:07:08 +0530

    First Commit

---
+++ apps/docs/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Admin } from "@repo/ui/admin";
+
+export default function(){
+    return <div>
+        Hii From Admin 
+        <Admin/>
+    </div>
+}
---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Admin } from "@repo/ui/admin";
+
+export default function(){
+    return <div>
+        Hii from Admin Side
+        <Admin/>
+    </div>
+}
